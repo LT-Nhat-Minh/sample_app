9d94fd9157164b0460ce201049cbd1ac9fd8960c
LT-Nhat-Minh <22520863@gm.uit.edu.vn>
2025-07-22 17:03:26 +0700

    Chapter_3_4_5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  private
+  def set_locale
+    locale = params[:locale].to_s
+    if locale.present? && I18n.available_locales.map(&:to_s).include?(locale)
+      I18n.locale = locale
+    else
+      I18n.locale = I18n.default_locale
+    end
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
 end
